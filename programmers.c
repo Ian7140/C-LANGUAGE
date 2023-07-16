d93424d4b087cbc138d60c9722f8eef459ec2b7f
Ian7140 <rlawnals7140@gmail.com>
2023-07-16 17:36:22 +0900

    1412

--- programmers.c
+++ programmers.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
+#include<string.h>
+char alphabet[30] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+int count[30]={0};
 int main()
 {
-    char number[105];
-    gets(number);
-    for(int i=0;i<strlen(number);i++)
+    char sentence[100];
+    int n,i;
+    gets(sentence);
+    for(i=0;i<strlen(sentence);i++)
     {
-        if(number[i]!=' ')
+        if(sentence[i] >= 'a' && sentence[i] <= 'z')
         {
-            printf("%c",number[i]);
+            count[sentence[i]-'a'] += 1;
         }
+    }
+
+    for(i=0;i<26;i++)
+    {
+        printf("%c:%d\n",alphabet[i],count[i]);
     }
     return 0;
 }
