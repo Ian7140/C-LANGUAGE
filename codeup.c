e5bee06687fd67653c1bf042ce2423efc46003d5
Ian7140 <rlawnals7140@gmail.com>
2023-07-20 08:56:23 +0900

    a부터 b까지 출력하기

--- codeup.c
+++ codeup.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int i,a,b;
+    scanf("%d %d",&a,&b);
+    if(a<=b)
     {
-        printf("%d ",i);
+        for(i=a;i<=b;i++)
+        {
+            printf("%d ",i);
+        }
+    }
+    if(a>b)
+    {
+        for(i=b;i<=a;i++)
+        {
+            printf("%d ",i);
+        }
     }
     return 0;
 }
