7ba8c8dfd5e10972dd8c3e0f904138252b05dcd4
Ian7140 <rlawnals7140@gmail.com>
2023-07-20 21:52:14 +0900

    I.O.I

--- codeup.c
+++ codeup.c
@@ -2,16 +2,21 @@
 #include<string.h>
 int main()
 {
-    char love[105];
-    int i,n=0;
-    gets(love);
-    for(i=0;i<strlen(love);i++)
+    int n,i;
+    char ioi[15];
+    scanf("%s",ioi);
+    for(i=0;i<strlen(ioi);i++)
     {
-        if(love[i]=='l' && love[i+1]=='o' && love[i+2]=='v' && love[i+3]=='e')
+        if(ioi[i]=='I' && ioi[i+1]=='O' && ioi[i+2]=='I' && strlen(ioi)==3)
         {
-            n++;
+            printf("IOI is the International Olympiad in Informatics.");
+            break;
+        }
+        else
+        {
+            printf("I don't care.");
+            break;
         }
     }
-    printf("%d",n);
     return 0;
 }
