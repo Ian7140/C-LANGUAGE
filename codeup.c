6168b9f7786ca2ecb24da4f8dfd9a0bc50b84e2e
Ian7140 <rlawnals7140@gmail.com>
2023-07-20 21:55:42 +0900

    welcome

--- codeup.c
+++ codeup.c
@@ -2,21 +2,8 @@
 #include<string.h>
 int main()
 {
-    int n,i;
-    char ioi[15];
-    scanf("%s",ioi);
-    for(i=0;i<strlen(ioi);i++)
-    {
-        if(ioi[i]=='I' && ioi[i+1]=='O' && ioi[i+2]=='I' && strlen(ioi)==3)
-        {
-            printf("IOI is the International Olympiad in Informatics.");
-            break;
-        }
-        else
-        {
-            printf("I don't care.");
-            break;
-        }
-    }
+    char name[10];
+    scanf("%s",name);
+    printf("welcome! %s",name);
     return 0;
 }
