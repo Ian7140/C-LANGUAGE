e0d2b59855dc711c954735893aab7e6a4aea06ed
Ian7140 <rlawnals7140@gmail.com>
2023-07-20 21:47:19 +0900

    love 2

--- codeup.c
+++ codeup.c
@@ -2,15 +2,16 @@
 #include<string.h>
 int main()
 {
-    char word[15];
-    int i;
-    scanf("%s",word);
-    for(i=0;i<strlen(word);i++)
+    char love[105];
+    int i,n=0;
+    gets(love);
+    for(i=0;i<strlen(love);i++)
     {
-        if(word[i]=='t')
+        if(love[i]=='l' && love[i+1]=='o' && love[i+2]=='v' && love[i+3]=='e')
         {
-            printf("%d ",i+1);
+            n++;
         }
     }
+    printf("%d",n);
     return 0;
 }
