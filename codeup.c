aab7448dee0087290d3cef2fec7d8a6376026c75
Ian7140 <rlawnals7140@gmail.com>
2023-07-22 16:54:33 +0900

    나이 계산1

--- codeup.c
+++ codeup.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-  char number[505];
-  int n=0,i;
-  scanf("%s",number);
-  for(i=0;i<strlen(number);i++)
-    {
-      n+=number[i]-'0';
-    }
-  if(n%3==0) printf("1");
-  else printf("0");
+  int a,b;
+  scanf("%06d %d",&a,&b);
+  a/=10000;
+  if(b==1 || b==2) printf("%d",2012-(1900+a)+1);
+  else if(b==3 || b==4) printf("%d",2012-(2000+a)+1);
   return 0;
 }
