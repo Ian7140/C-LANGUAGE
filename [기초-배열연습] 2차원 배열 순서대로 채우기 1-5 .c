6e6eb2ab99c2b78219c734120022769b631e603e
김민주 <rlawnals7140@gmail.com>
2023-08-08 20:28:47 +0900

    1465

---
+++ [기초-배열연습] 2차원 배열 순서대로 채우기 1-5 .c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int number[101][101];
+int main()
+{
+  int n,m,i,j,count=1;
+  scanf("%d %d",&n,&m);
+  for(i=0;i<n;i++)
+    {
+      for(j=0;j<m;j++)
+        {
+          number[i][j]=count++;
+        }
+    }// 배열에 값 넣기
+
+  for(i=n-1;i>=0;i--)
+    {
+      for(j=m-1;j>=0;j--)
+        {
+          printf("%d ",number[i][j]);
+        }
+      printf("\n");
+    }
+  return 0;
+}
